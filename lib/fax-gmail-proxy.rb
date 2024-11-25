dd28e09d8d27ee5f896b910a572782000be944e2
takuya <55338+takuya@users.noreply.github.com>
2024-11-26 00:06:56 +0900

    整形

--- lib/fax-gmail-proxy.rb
+++ lib/fax-gmail-proxy.rb
@@ -1,17 +1,19 @@
 require 'takuya/gmail-forwarder'
 require 'BrotherFaxMessage'
 
-class FaxGmailProxy <Takuya::GMailForwarderServer
+class FaxGmailProxy<Takuya::GMailForwarderServer
   def initialize(user_id: nil, password: nil, client_secret_path: nil, token_path: nil, **args)
     super
-    @working =false
+    @working = false
   end
+
   ## override
   def serve_client(session, io)
     @working = true
     super(session, io)
     @working = false
   end
+
   def stop(wait_seconds_before_close: nil, gracefully: nil)
     while processing? do
       sleep 1
@@ -19,6 +21,7 @@ class FaxGmailProxy <Takuya::GMailForwarderServer
     super
     sleep 0.3 until stopped?
   end
+
   def processing?
     @working
   end
@@ -36,17 +39,17 @@ class FaxGmailProxy <Takuya::GMailForwarderServer
     modified_mail = self.class.modify_mail(original_mail)
     [envelope_from, envelope_to, modified_mail]
   end
+
   # @param mail [Mail::Message]
   def is_tiff_fax_mail?(mail)
-    mail.attachments.any?{|a| a.content_type&& a.content_type =~ /tiff/  } ||
-      mail.attachments.any?{|a|  a.content_disposition &&  a.content_disposition =~/tiff;/ }
+    mail.attachments.any? { |a| a.content_type && a.content_type=~/tiff/ } ||
+      mail.attachments.any? { |a| a.content_disposition && a.content_disposition=~/tiff;/ }
   end
+
   # @param mail [Mail::Message]
   def is_image_attached_mail?(mail)
-      mail.attachments.any?{|a|  a.content_disposition &&  a.content_disposition =~/image;/ }
+    mail.attachments.any? { |a| a.content_disposition && a.content_disposition=~/image;/ }
   end
-
-
 
   # @return mail [Mail::Message]
   # @param original_mail [Mail::Message]
